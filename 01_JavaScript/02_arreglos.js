907a6169361a19260f5517ceca55563fa680b798
Criss Defaz <LeAdmiCriss001@outloo.es>
2020-06-16 18:09:50 -0500

    Variables y arreglos

---
+++ 01_JavaScript/02_arreglos.js
@@ -0,0 +1,13 @@
+const arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// arreglo = 1; // inmutable, no se puede reasignar.
+let cualquierCosa = "ASD"
+/*
+cualquierCosa = 1
+cualquierCosa = true
+cualquierCosa = undefined
+cualquierCosa = null
+ */
+// En JS pueden ser de cualquier tipo de datos
+const arregloTodo = [
+    true, 1, 1.2, "Criss", undefined, null, {},[1, 2, true, "A"]
+]
